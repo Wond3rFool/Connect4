81685fae2963c017ae503bb7373c1200d3c08b16
Tiago V <T.Verstoep96@gmail.com>
2023-12-18 12:22:07 +0100

    Made Prefabs and Start for placement
    Made the objects that need to be placed and a start to how placement is going to work.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
@@ -15,6 +18,17 @@ public class InputManager : MonoBehaviour
 
     private Vector3 lastPosition;
 
+    public event Action OnClicked, OnExit;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) 
+            OnClicked?.Invoke();
+        if(Input.GetKey(KeyCode.Escape))
+            OnExit?.Invoke();
+    }
+
+    public bool IsPointerOverUI() => EventSystem.current.IsPointerOverGameObject();
 
     public Vector3 GetSelectedMapPosition()
     {
--- Assets/Scripts/PlacementSystem.cs
+++ Assets/Scripts/PlacementSystem.cs
@@ -11,9 +11,49 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField]
     private Grid grid;
 
+    [SerializeField]
+    private ObjectDatabaseSO database;
+    private int selectedObjectIndex = -1;
 
+
+
+    private void Start() 
+    {
+        StopPlacement();
+    }
+    public void StartPlacement(int ID) 
+    {
+        StopPlacement();
+        selectedObjectIndex = database.objects.FindIndex(data => data.ID == ID);
+        if(selectedObjectIndex < 0) 
+        {
+            Debug.LogError($"no ID found {ID}");
+            return;
+        }
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceObject;
+        inputManager.OnExit += StopPlacement;
+    }
+    private void StopPlacement() 
+    {
+        selectedObjectIndex = -1;
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceObject;
+        inputManager.OnExit -= StopPlacement;
+    }
+    private void PlaceObject()
+    {
+        if (inputManager.IsPointerOverUI()) return;
+
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        GameObject newSphere = Instantiate(database.objects[selectedObjectIndex].Prefab);
+        newSphere.transform.position = grid.CellToWorld(gridPosition);
+
+    }
     private void Update() 
     {
+        if (selectedObjectIndex < 0) return;
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
